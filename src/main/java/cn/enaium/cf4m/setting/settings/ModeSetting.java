a498944e952b5e2dabce6ea674c6909cd623a179
Enaium <32991121+Enaium@users.noreply.github.com>
2020-05-06 15:44:51 +0800

    Create ModeSetting.java

---
+++ src/main/java/cn/enaium/cf4m/setting/settings/ModeSetting.java
@@ -0,0 +1,32 @@
+package cn.enaium.cf4m.setting.settings;
+
+import cn.enaium.cf4m.module.Module;
+import cn.enaium.cf4m.setting.Setting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Project: cf4m
+ * -----------------------------------------------------------
+ * Copyright © 2020 | Enaium | All rights reserved.
+ */
+public class ModeSetting extends Setting {
+
+    private String current;
+    private List<String> modes;
+
+    public ModeSetting(Module module, String name, String info, String current, List<String> modes) {
+        super(module, name, info);
+        this.current = current;
+        this.modes = modes;
+    }
+
+    public String getCurrent() {
+        return current;
+    }
+
+    public void setCurrent(String current) {
+        this.current = current;
+    }
+}
