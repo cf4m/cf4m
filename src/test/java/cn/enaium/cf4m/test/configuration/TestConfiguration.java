05dd1f8d37251d1b67b6ac7262d2774f986fdccf
Enaium <32991121+Enaium@users.noreply.github.com>
2021-02-16 11:14:48 +0800

    WORLD

---
+++ src/test/java/cn/enaium/cf4m/test/configuration/TestConfiguration.java
@@ -0,0 +1,14 @@
+package cn.enaium.cf4m.test.configuration;
+
+import cn.enaium.cf4m.annotation.Configuration;
+import cn.enaium.cf4m.configuration.IConfiguration;
+
+/**
+ * Project: cf4m
+ * -----------------------------------------------------------
+ * Copyright © 2020-2021 | Enaium | All rights reserved.
+ */
+@Configuration
+public enum TestConfiguration implements IConfiguration {
+
+}
