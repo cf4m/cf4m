20970fcd9e195c8d9b0e82ee0eff1393e0dac18a
Enaium <32991121+Enaium@users.noreply.github.com>
2021-02-27 15:59:27 +0800

    Update EventManager.java

--- src/main/java/cn/enaium/cf4m/manager/EventManager.java
+++ src/main/java/cn/enaium/cf4m/manager/EventManager.java
@@ -2,13 +2,10 @@ package cn.enaium.cf4m.manager;
 
 import cn.enaium.cf4m.annotation.Event;
 import cn.enaium.cf4m.event.Listener;
-import cn.enaium.cf4m.event.MethodBean;
-import com.google.common.collect.ArrayListMultimap;
+import cn.enaium.cf4m.event.EventBean;
 import com.google.common.collect.Maps;
-import com.google.common.collect.Multimap;
 
 import java.lang.reflect.Method;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -25,7 +22,7 @@ public class EventManager {
      * <K> listener
      * <V> event
      */
-    private final HashMap<Class<? extends Listener>, CopyOnWriteArrayList<MethodBean>> events;
+    private final HashMap<Class<? extends Listener>, CopyOnWriteArrayList<EventBean>> events;
 
     public EventManager() {
         events = Maps.newHashMap();
@@ -44,17 +41,17 @@ public class EventManager {
                 method.setAccessible(true);
                 @SuppressWarnings("unchecked")
                 Class<? extends Listener> listener = (Class<? extends Listener>) method.getParameterTypes()[0];
-                MethodBean methodBean = new MethodBean(o, method, method.getAnnotation(Event.class).priority());
+                EventBean eventBean = new EventBean(o, method, method.getAnnotation(Event.class).priority());
 
                 if (events.containsKey(listener)) {
-                    if (!events.get(listener).contains(methodBean)) {
-                        events.get(listener).add(methodBean);
+                    if (!events.get(listener).contains(eventBean)) {
+                        events.get(listener).add(eventBean);
                     }
                 } else {
-                    events.put(listener, new CopyOnWriteArrayList<>(Collections.singletonList(methodBean)));
+                    events.put(listener, new CopyOnWriteArrayList<>(Collections.singletonList(eventBean)));
                 }
 
-                events.values().forEach(methodBeans -> methodBeans.sort((Comparator.comparingInt(MethodBean::getPriority))));
+                events.values().forEach(methodBeans -> methodBeans.sort((Comparator.comparingInt(EventBean::getPriority))));
             }
         }
     }
@@ -65,11 +62,11 @@ public class EventManager {
      * @param o Object
      */
     public void unregister(Object o) {
-        events.values().forEach(methodBeans -> methodBeans.removeIf(methodMethodBean -> methodMethodBean.getObject().equals(o)));
+        events.values().forEach(methodBeans -> methodBeans.removeIf(methodEventBean -> methodEventBean.getObject().equals(o)));
         events.entrySet().removeIf(event -> event.getValue().isEmpty());
     }
 
-    public CopyOnWriteArrayList<MethodBean> getEvent(Class<? extends Listener> type) {
+    public CopyOnWriteArrayList<EventBean> getEvent(Class<? extends Listener> type) {
         return events.get(type);
     }
 }
