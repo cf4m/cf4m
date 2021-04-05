61d33a2e0d6216c0791a408fcc4523aee042bec4
Enaium <32991121+Enaium@users.noreply.github.com>
2021-04-05 18:33:30 +0800

    Update Auto.java

--- src/main/java/cn/enaium/cf4m/annotation/Auto.java
+++ src/main/java/cn/enaium/cf4m/annotation/Auto.java
@@ -7,6 +7,28 @@ import java.lang.annotation.Target;
 
 /**
  * Add this annotation to the class/field to the field of this class will be auto put
+ * <p>
+ * Support class {@link cn.enaium.cf4m.annotation.module.Module}
+ * <p>
+ * Support class {@link cn.enaium.cf4m.annotation.command.Command}
+ * <p>
+ * Support class {@link cn.enaium.cf4m.annotation.config.Config}
+ * <p>
+ * Support field {@link cn.enaium.cf4m.container.EventContainer}
+ * <p>
+ * Support field {@link cn.enaium.cf4m.container.ModuleContainer}
+ * <p>
+ * Support field {@link cn.enaium.cf4m.container.CommandContainer}
+ * <p>
+ * Support field {@link cn.enaium.cf4m.container.ConfigContainer}
+ * <p>
+ * Support field {@link cn.enaium.cf4m.configuration.IConfiguration}
+ * <p>
+ * Support field {@link cn.enaium.cf4m.provider.ModuleProvider}
+ * <p>
+ * Support field {@link cn.enaium.cf4m.provider.CommandProvider}
+ * <p>
+ * Support field {@link cn.enaium.cf4m.provider.ConfigProvider}
  *
  * @author Enaium
  */
