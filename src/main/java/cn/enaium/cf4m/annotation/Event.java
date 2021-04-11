c91bfd48d78bc4c893c765e102a14a406e435032
Enaium <32991121+Enaium@users.noreply.github.com>
2021-04-11 15:19:15 +0800

    rename

--- src/main/java/cn/enaium/cf4m/annotation/Event.java
+++ src/main/java/cn/enaium/cf4m/annotation/Event.java
@@ -1,9 +1,6 @@
 package cn.enaium.cf4m.annotation;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  * Add this annotation to the method to that this method will be invoke at event fire
