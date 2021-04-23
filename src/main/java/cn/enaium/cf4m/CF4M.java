4f87e97aed437803c5fe78ae4eeb3427e9b6b2c6
Enaium <32991121+Enaium@users.noreply.github.com>
2021-04-23 20:20:29 +0800

    Update CF4M.java

--- src/main/java/cn/enaium/cf4m/CF4M.java
+++ src/main/java/cn/enaium/cf4m/CF4M.java
@@ -13,51 +13,6 @@ public final class CF4M {
 
     public static ICF4M INSTANCE;
 
-    public CF4M(Class<?> mainClass, String dir) {
-        final ClassContainer classContainer = new ClassBuilder(mainClass).classContainer;
-        final EventContainer eventContainer = new EventBuilder(classContainer).eventContainer;
-        final IConfiguration configuration = new ConfigurationBuilder(classContainer).configuration;
-        final ModuleContainer moduleContainer = new ModuleBuilder(classContainer).moduleContainer;
-        final CommandContainer commandContainer = new CommandBuilder(classContainer, configuration).commandContainer;
-        final ConfigContainer configContainer = new ConfigBuilder(classContainer, configuration, dir).configContainer;
-        INSTANCE = new ICF4M() {
-            @Override
-            public ClassContainer getKlass() {
-                return classContainer;
-            }
-
-            @Override
-            public EventContainer getEvent() {
-                return eventContainer;
-            }
-
-            @Override
-            public IConfiguration getConfiguration() {
-                return configuration;
-            }
-
-            @Override
-            public ModuleContainer getModule() {
-                return moduleContainer;
-            }
-
-            @Override
-            public CommandContainer getCommand() {
-                return commandContainer;
-            }
-
-            @Override
-            public ConfigContainer getConfig() {
-                return configContainer;
-            }
-        };
-        classContainer.after();
-    }
-
-    public void useless() {
-
-    }
-
     private static boolean run = false;
 
     /**
@@ -68,7 +23,46 @@ public final class CF4M {
         if (run) {
             new Exception("CF4M already run").printStackTrace();
         } else {
-            new CF4M(mainClass, path).useless();
+
+            final ClassContainer classContainer = new ClassBuilder(mainClass).classContainer;
+            final EventContainer eventContainer = new EventBuilder(classContainer).eventContainer;
+            final IConfiguration configuration = new ConfigurationBuilder(classContainer).configuration;
+            final ModuleContainer moduleContainer = new ModuleBuilder(classContainer).moduleContainer;
+            final CommandContainer commandContainer = new CommandBuilder(classContainer, configuration).commandContainer;
+            final ConfigContainer configContainer = new ConfigBuilder(classContainer, configuration, path).configContainer;
+            INSTANCE = new ICF4M() {
+                @Override
+                public ClassContainer getKlass() {
+                    return classContainer;
+                }
+
+                @Override
+                public EventContainer getEvent() {
+                    return eventContainer;
+                }
+
+                @Override
+                public IConfiguration getConfiguration() {
+                    return configuration;
+                }
+
+                @Override
+                public ModuleContainer getModule() {
+                    return moduleContainer;
+                }
+
+                @Override
+                public CommandContainer getCommand() {
+                    return commandContainer;
+                }
+
+                @Override
+                public ConfigContainer getConfig() {
+                    return configContainer;
+                }
+            };
+            classContainer.after();
+
             INSTANCE.getConfig().load();
             Runtime.getRuntime().addShutdownHook(new Thread(() -> INSTANCE.getConfig().save()));
             run = true;
