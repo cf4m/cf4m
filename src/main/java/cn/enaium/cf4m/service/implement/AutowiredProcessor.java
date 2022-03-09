1fafcab232ca4a9b3a5f51fa0946e0ad33470f78
Enaium <wyy-666@hotmail.com>
2022-03-09 22:35:17 +0800

    service autowired

--- src/main/java/cn/enaium/cf4m/service/implement/AutowiredProcessor.java
+++ src/main/java/cn/enaium/cf4m/service/implement/AutowiredProcessor.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright (C) 2020 Enaium
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package cn.enaium.cf4m.service.implement;
 
 import cn.enaium.cf4m.CF4M;
@@ -39,12 +55,7 @@ public class AutowiredProcessor implements ClassService {
             try {
                 CF4M.CLASS.getService(AutowiredService.class).forEach(postProcessor -> postProcessor.beforePut(field, instance));
                 if (field.get(instance) == null) {
-                    Object o = CF4M.CLASS.getBeans().get(field.getType());
-                    if (o == null && !CF4M.CLASS.getService(field.getType()).isEmpty()) {
-                        o = CF4M.CLASS.getService(field.getType()).get(0);
-                    }
-
-                    field.set(instance, o);
+                    field.set(instance, CF4M.CLASS.getBeans().get(field.getType()));
                 }
                 CF4M.CLASS.getService(AutowiredService.class).forEach(autowiredService -> autowiredService.afterPut(field, instance));
             } catch (IllegalAccessException e) {
