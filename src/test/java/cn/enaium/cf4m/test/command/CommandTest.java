151b9290950a157d1fff1e5b1cbd1491fdef140c
Enaium <wyy-666@hotmail.com>
2022-03-10 13:51:46 +0800

    Update CommandTest.java

--- src/test/java/cn/enaium/cf4m/test/command/CommandTest.java
+++ src/test/java/cn/enaium/cf4m/test/command/CommandTest.java
@@ -1,17 +1,25 @@
 package cn.enaium.cf4m.test.command;
 
 import cn.enaium.cf4m.CF4M;
+import cn.enaium.cf4m.annotation.Autowired;
 import cn.enaium.cf4m.annotation.command.Command;
 import cn.enaium.cf4m.annotation.command.Exec;
 import cn.enaium.cf4m.configuration.CommandConfiguration;
+import cn.enaium.cf4m.container.ClassContainer;
+import org.junit.jupiter.api.Assertions;
 
 /**
  * @author Enaium
  */
 @Command({"t", "test"})
 public class CommandTest {
+
+    @Autowired
+    private ClassContainer classContainer;
+
     @Exec
     public void exec() {
+        Assertions.assertNotNull(classContainer);
         CF4M.CONFIGURATION.get(CommandConfiguration.class).message("No var");
     }
 
